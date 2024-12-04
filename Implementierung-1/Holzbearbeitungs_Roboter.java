a16ae28fd00ea54c6da60f1d2e16922edc56b7c3
silvandario <silvan.dario@hotmail.com>
2024-12-04 12:31:25 +0100

    fügt Infos aus Übung & neue Klassen hinzu
    nur eine Klasse, die von Roboter erbt, sei notwendig

---
+++ Implementierung-1/Holzbearbeitungs_Roboter.java
@@ -0,0 +1,35 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Holzbearbeitungs_Roboter.
+ * 
+ * @author Silvan
+ * @version 1
+ * 
+ * * In Übungslektion bei Alex:  nur eine Variante ist notwendig, einfach zum zeigen, dass es funktioniert
+ */
+public class Holzbearbeitungs_Roboter extends Roboter
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Holzbearbeitungs_Roboter
+     */
+    public Holzbearbeitungs_Roboter()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
---
+++ Implementierung-1/Roboter.java
@@ -0,0 +1,36 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Roboter.
+ * 
+ * @author Silvan
+ * @version 1
+ * 
+ * In Übungslektion bei Alex: Montage_roboter, etc. -> nur eine Variante ist notwendig, einfach zum zeigen, dass es funktioniert
+ */
+
+public class Roboter
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Roboter
+     */
+    public Roboter()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
